fc381dc22cfe19f88247ed4d5901978b4747fd98
MinhLong2410-02 <minhlong2002@gmail.com>
2024-03-17 00:08:58 +0700

    integrate chatbot

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 tr = Async_PyTranslator()
 app = FastAPI()
+llm = Chatbot()
 
 @app.get("/get_user_tokens")
 def get_user_tokens(user_login: UserLogin):
@@ -30,20 +31,24 @@ def get_user_tokens(user_login: UserLogin):
     return {"token": token}
 
 @app.post("/getstream_webhooks")
-def getstream_webhooks(request: Request):
+async def getstream_webhooks(request: Request):
     valid = server_client.verify_webhook(request.body, request.headers.get("x-signature"))
     if not valid:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid signature")
 
-    data = request.json()
+    data = await request.json()
     if 'message' in data:
         message_content = data['message']
+        user_id = message_content['user_id']
         channel_type, channel_id = message_content['cid'].split(":")
+
         if 'command' in message_content:
             command = message_content['command']
+            session_id = user_id    
             args = message_content['args']
+            response = await llm.ask(args, session_id=session_id) 
             channel = server_client.channel(channel_type=channel_type, channel_id=channel_id)
-            channel.send_message({"text": f"Result: {eval(args)}"}, message_content['user_id'])
+            channel.send_message({"text": response}, message_content['user_id'])
             return {"result": "command executed"}
 
     return {"data": data}
@@ -54,21 +59,20 @@ def webhooks_health_check():
     return {"health status": 'excellent'}
 
 
-llm = Chatbot()
 
-async def generator(query: str, session_id: str, internet_search:bool) -> AsyncGenerator[str, None]:
-    content = ""
-    query = await tr.translate_dict(query, "en")
-    print(query)
-    for chunk in llm.stream(query['translation'], session_id, internet_search=internet_search):
-        content += chunk
-        if "\n" in chunk:
-            res =  await tr.translate_dict(content, "vi")
-            yield res['translation']+'\n'
-            content = ""
-    if content != "":
-        res =  await tr.translate_dict(content, "vi")
-        yield res['translation']+'\n'
+# async def generator(query: str, session_id: str, internet_search:bool) -> AsyncGenerator[str, None]:
+#     content = ""
+#     query = await tr.translate_dict(query, "en")
+#     print(query)
+#     for chunk in llm.stream(query['translation'], session_id, internet_search=internet_search):
+#         content += chunk
+#         if "\n" in chunk:
+#             res =  await tr.translate_dict(content, "vi")
+#             yield res['translation']+'\n'
+#             content = ""
+#     if content != "":
+#         res =  await tr.translate_dict(content, "vi")
+#         yield res['translation']+'\n'
 
 # @app.get("/chat/")
 # async def chat(query: str,internet_search: bool, current_user: User = Depends(get_current_active_user), session_id: str = ""):
